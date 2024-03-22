ff2208100c9d341a5241e0a8ec6188963e97c1b9
axelsson <aa255tr@student.lnu.se>
2024-03-22 22:03:27 +0100

    Fixed that the removal of some files was not actually done, also that the database separation was not done either.

--- .packages/github.com/toitware/toit-dhtxx/1.4.0/src/dhtxx.toit
+++
@@ -1,9 +0,0 @@
-// Copyright (C) 2022 Toitware ApS. All rights reserved.
-// Use of this source code is governed by an MIT-style license that can be found
-// in the LICENSE file.
-
-import .dht11 show Dht11
-import .dht22 show Dht22
-import .driver_ show DhtResult
-
-export *
--- Database/MQTT_DB_Connector.py
+++ Database/MQTT_DB_Connector.py
@@ -1,24 +1,19 @@
-#!/usr/bin/python3
 import base64
-import mysql.connector, re, time
+import mysql.connector
 from mysql.connector import errorcode
-import os, sys, logging, time
+import sys
 import paho.mqtt.client as mqtt
 import json
 from datetime import datetime
-import sys
-import sqlparse
 
-
-mysqlConnected = False
-mysql_username = ""
-mysql_password = ""
-mysql_host_ip  = ""
-mysql_socket_path   = ""
+mysqlConnected    = False
+mysql_username    = ""
+mysql_password    = ""
+mysql_host_ip     = ""
+mysql_socket_path = ""
 sqlFile = open("iotstormdrain.sql").read()
 
 try:
-
     if len(sys.argv) < 5:
         print ("Not enough arguments was given.")
         print ("Example usage: python MQTT_DB_Connector [Database name] [mysqlusername] [mysqlpassword] [host-ip] [mysql-socket-path]")
@@ -47,24 +42,22 @@ try:
 
         mqttUsername = input("Enter MQTT-connection username. ")
         mqttPassword = input("Enter MQTT-connection password. ")
-#'/Applications/MAMP/tmp/mysql/mysql.sock'
 
 except Exception as err:
     print(err)
-
 
 cnx = mysql.connector.connect(user = mysql_username,
                               password = mysql_password,
                               host = mysql_host_ip,
                               unix_socket = mysql_socket_path,
                               )
-
-# DB_NAME = 'iotstormdrain'
 cursor = cnx.cursor()
+theRegion = region	     # The region the user is using
 
-User = mqttUsername#"iotwaterlevelstormdrain@ttn"
-Password = mqttPassword #"NNSXS.5NU63ND6GOZDI4I5I7QJEDOZ6I6QMJZVQ56VL6Q.XC5JWWVELD5FHEMOYMLYPHP43H7LZJF7GCRZUOPKQUJ6GLYMJLAQ"
-theRegion = region#"EU1"		# The region you are using
+# Credentials for the MQTT Connection
+User = mqttUsername      
+Password = mqttPassword
+
 
 # This function creates the database
 def create_database(cursor, DB_NAME):
@@ -114,37 +107,24 @@ def insert_into_table(cursor, insertTable, values):
         print("OK")
 
 
-# Write uplink to tab file
-def saveToFile(someJSON):
+# Write the uplink message into the Database
+def saveToDB(someJSON):
   end_device_ids = someJSON["end_device_ids"]
   device_id = end_device_ids["device_id"]
   application_id = end_device_ids["application_ids"]["application_id"]
-  
-  received_at = someJSON["received_at"]
-  
+
   try:
-      
     uplink_message = someJSON["uplink_message"];
-    # f_port = uplink_message["f_port"];
-    # f_cnt = uplink_message["f_cnt"];
     frm_payload = uplink_message["frm_payload"];
     rssi = uplink_message["rx_metadata"][0]["rssi"];
     snr = uplink_message["rx_metadata"][0]["snr"];
     data_rate = uplink_message["settings"]["data_rate"];
-    # consumed_airtime = uplink_message["consumed_airtime"];	
-    frm_payload = base64.b64decode(frm_payload).hex()#' '.join([ str(ord(c)) for c in frm_payload.decode('base64') ])
-    # frm_payload = frm_payload[4:]
-    # Daily log of uplinks
+    frm_payload = base64.b64decode(frm_payload).hex()
 
-    payload_dec = int(bin(int(frm_payload, 16)), 2)
-    # print(int(bin(int(frm_payload, 16)), 2))
-    print ("payload_dec:", payload_dec) 
+
+    payload_dec = int(bin(int(frm_payload, 16)), 2) # Decoding the Hexadecimal payload into decmial
       
     now = datetime.now()
-    # pathNFile = now.strftime("%Y%m%d") + ".txt"
-    
-    #Values("Rodia", "29", "305", "7549", "hot", "1 standard", "jungles, oceans, urban, swamps", "60", "")
-    # "INSERT INTO logs (date, endDevice_id, device_id, app_id, uplink_msg, frm_payload, rssi, snr, data_rate)"
     dataValues = "Values('%s','%s','%s','%s','%s',%d)" % (now, device_id, application_id, int(str(payload_dec)[1:len(str(payload_dec))]), rssi, snr)
     print ("\n\n" + dataValues)
     
@@ -165,33 +145,29 @@ def on_connect(mqttc, obj, flags, rc):
     print("\nConnect: rc = " + str(rc))
 
 def on_message(mqttc, obj, msg):
-    print("\nMessage: " + msg.topic + " " + str(msg.qos)) # + " " + str(msg.payload))
+    print("\nMessage: " + msg.topic + " " + str(msg.qos))
     parsedJSON = json.loads(msg.payload)
-    print(json.dumps(parsedJSON, indent=4))	# Uncomment this to fill your terminal screen with JSON
-    saveToFile(parsedJSON)
+    saveToDB(parsedJSON)
 
 def on_subscribe(mqttc, obj, mid, granted_qos):
     print("\nSubscribe: " + str(mid) + " " + str(granted_qos))
 
-def on_log(mqttc, obj, level, string):
-    print("\nLog: "+ string)
-    logging_level = mqtt.LOGGING_LEVEL[level]
-    logging.log(logging_level, string)
+# def on_log(level, string):
+#     print("\nLog: "+ string)
+#     logging_level = mqtt.LOGGING_LEVEL[level]
+#     logging.log(logging_level, string)
 
 
-
-print("Body of program:")
-
-print("Init mqtt client")
+print("Initializing the MQTT Client")
 mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION1)
 
-print("Assign callbacks")
+print("Assigning callback functions.")
 mqttc.on_connect = on_connect
 mqttc.on_subscribe = on_subscribe
 mqttc.on_message = on_message
 #mqttc.on_log = on_log		# Logging for debugging OK, waste
 
-print("Connect")
+print("Connecting..")
 # Setup authentication from settings above
 mqttc.username_pw_set(User, Password)
 
@@ -201,22 +177,18 @@ mqttc.tls_set()	# default certification authority of the system
 
 mqttc.connect(theRegion.lower() + ".cloud.thethings.network", 8883, 60)
 # mqttc.connect(connectString, 8883, 60)
-
-print("Subscribe")
 mqttc.subscribe("#", 0)	# all device uplinks
 
-
-
-print("And run forever")
 try:    
   run = True
+  print("Connected to and listening to the MQTT Connection..")
   while run:
-    mqttc.loop(10) 	# seconds timeout / blocking time
+    mqttc.loop(10) 	# timeout
     print(".", end="", flush=True)	# feedback to the user that something is actually happening
     
     
 except KeyboardInterrupt:
-    print("Exit")
+    print("Exiting")
     cursor.close()
     cnx.close()
     sys.exit(0)
